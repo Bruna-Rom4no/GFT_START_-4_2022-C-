be2c1c3ad0b6ba7b18118a644d57979b094e3ece
Bruna Romano <85489185+Bruna-Rom4no@users.noreply.github.com>
2022-04-08 17:41:56 -0300

    Add files via upload

---
+++ GFT_EXAME/GFT_EXAME/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GFT_EXAME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            List<int> _ordem = new List<int>();
+
+            while (i <= 2)
+            {
+                Console.WriteLine("Digite o " + (i + 1) + " valor");
+                _ordem.Add(int.Parse(Console.ReadLine()));
+
+                i++;
+            }
+
+            _ordem.Sort();
+            _ordem.Reverse();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Os valores que foram digitados são: ");
+
+            foreach (int ordem in _ordem)
+            {
+                Console.WriteLine(ordem);
+            }
+            Console.ReadKey();
+        }
+    
+    }
+}
---
+++ GFT_EXAME_Exercicio4/GFT_EXAME_Exercicio4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GFT_EXAME_Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome: ");
+            string cliente1 = Console.ReadLine();
+            Console.WriteLine("Valor de compra: ");
+            int Compra1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome: ");
+            string cliente2 = Console.ReadLine();
+            Console.WriteLine("Valor de compra: ");
+            int Compra2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome: ");
+            string cliente3 = Console.ReadLine();
+            Console.WriteLine("Valor de compra: ");
+            int Compra3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome: ");
+            string cliente4 = Console.ReadLine();
+            Console.WriteLine("Valor de compra: ");
+            int Compra4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome: ");
+            string cliente5 = Console.ReadLine();
+            Console.WriteLine("Valor de compra: ");
+            int Compra5 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome: ");
+            string cliente6 = Console.ReadLine();
+            Console.WriteLine("Valor de compra: ");
+            int Compra6 = int.Parse(Console.ReadLine());
+
+            int faturamento = Compra1 + Compra2 + Compra3 + Compra4 + Compra5 + Compra6;
+
+            if (faturamento > 54000)
+            {
+                int total = faturamento - 54000;
+                Console.WriteLine($"O faturamento superou a loja de Ubiratã em {total}");
+            }
+        }
+    }
+}
