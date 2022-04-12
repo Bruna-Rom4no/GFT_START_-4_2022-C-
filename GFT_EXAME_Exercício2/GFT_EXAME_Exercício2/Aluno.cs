c04417c250508fc5ae019aaba4845b581b1a904f
Bruna Romano <85489185+Bruna-Rom4no@users.noreply.github.com>
2022-04-12 01:18:24 -0300

    Add files via upload

---
+++ GFT_EXAME_Exercício2/GFT_EXAME_Exercício2/Aluno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFT_EXAME_Exercício2
+{
+    class Aluno
+    {
+        string matricula;
+
+        int nota1;
+        int nota2;
+        int nota3;
+
+        float faltas;
+
+        public Aluno(string matricula, int nota1, int nota2, int nota3, float faltas)
+        {
+            this.matricula = matricula;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+            this.nota3 = nota3;
+            this.faltas = faltas;
+        }
+
+        public Aluno()
+        {
+
+        }
+
+        public int Media()
+        {
+            int total;
+
+            return total = (nota1 + nota2 + nota3) / 3;
+        }
+
+        public float Frequencia()
+        {
+            float CargaHoraAulas = 1000;
+
+            return (faltas / CargaHoraAulas) * 100;
+        }
+    }
+}
---
+++ GFT_EXAME_Exercício2/GFT_EXAME_Exercício2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GFT_EXAME_Exercício2
+{
+    class Program : Aluno
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno("123", 8, 9, 10, 250);
+            Aluno a2 = new Aluno("456", 5, 4, 6, 467);
+            Aluno a3 = new Aluno("456", 10, 5, 7, 870);
+            Aluno a4 = new Aluno("456", 8, 9, 6, 590);
+            Aluno a5 = new Aluno("456", 9, 9, 10, 978);
+
+            Console.WriteLine($"O aluno 1 tem média {a1.Media()} e frequentou {a1.Frequencia()}% das aulas.");
+
+            Console.WriteLine($"O aluno 2 tem média {a2.Media()} e frequentou {a2.Frequencia()}% das aulas.");
+
+            Console.WriteLine($"O aluno 3 tem média {a3.Media()} e frequentou {a3.Frequencia()}% das aulas.");
+
+            Console.WriteLine($"O aluno 4 tem média {a4.Media()} e frequentou {a4.Frequencia()}% das aulas.");
+
+            Console.WriteLine($"O aluno 5 tem média {a5.Media()} e frequentou {a5.Frequencia()}% das aulas.");
+        }
+    }
+}
